9456235cf2e30a317ab31553e72f861c223cd091
The GIT SLAYER <44425998+DarkDemonCatX256@users.noreply.github.com>
2019-04-10 11:30:16 +0200

    BetServer Manager first draft
    This program will eventually interface with the MySQL database via localhost or an online host of your choosing.
    Much more needs to be added.
    Doesn't it look cool?

---
+++ BetServerManager/BetServerManager/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace BetServerManager
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.connectionStateLabel = new System.Windows.Forms.Label();
+            this.clientCountLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.connectToSeverButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.userListView = new System.Windows.Forms.ListView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serverConnectionPicker = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(16, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server Connection Status :";
+            // 
+            // connectionStateLabel
+            // 
+            this.connectionStateLabel.AutoSize = true;
+            this.connectionStateLabel.ForeColor = System.Drawing.Color.Red;
+            this.connectionStateLabel.Location = new System.Drawing.Point(200, 18);
+            this.connectionStateLabel.Name = "connectionStateLabel";
+            this.connectionStateLabel.Size = new System.Drawing.Size(185, 17);
+            this.connectionStateLabel.TabIndex = 1;
+            this.connectionStateLabel.Text = "Connection State Undefined";
+            // 
+            // clientCountLabel
+            // 
+            this.clientCountLabel.AutoSize = true;
+            this.clientCountLabel.ForeColor = System.Drawing.Color.Red;
+            this.clientCountLabel.Location = new System.Drawing.Point(200, 35);
+            this.clientCountLabel.Name = "clientCountLabel";
+            this.clientCountLabel.Size = new System.Drawing.Size(164, 17);
+            this.clientCountLabel.TabIndex = 3;
+            this.clientCountLabel.Text = "Client Count Uninitailized";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(16, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Active Clients :";
+            // 
+            // connectToSeverButton
+            // 
+            this.connectToSeverButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.connectToSeverButton.ForeColor = System.Drawing.Color.Red;
+            this.connectToSeverButton.Location = new System.Drawing.Point(19, 532);
+            this.connectToSeverButton.Name = "connectToSeverButton";
+            this.connectToSeverButton.Size = new System.Drawing.Size(591, 47);
+            this.connectToSeverButton.TabIndex = 4;
+            this.connectToSeverButton.Text = "Connect To Database";
+            this.connectToSeverButton.UseVisualStyleBackColor = true;
+            this.connectToSeverButton.Click += new System.EventHandler(this.connectToSeverButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.connectionStateLabel);
+            this.groupBox1.Controls.Add(this.clientCountLabel);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox1.Location = new System.Drawing.Point(19, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(591, 69);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.userListView);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.serverConnectionPicker);
+            this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox2.ForeColor = System.Drawing.Color.Red;
+            this.groupBox2.Location = new System.Drawing.Point(19, 88);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(591, 438);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Settings";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(437, 197);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(101, 17);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "None Selected";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(437, 167);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(16, 17);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(437, 137);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(305, 197);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(128, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Selected Team(s) :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(305, 167);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Bet Ammount:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(305, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Cash(EUR) :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(305, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "User Data :";
+            // 
+            // userListView
+            // 
+            this.userListView.BackColor = System.Drawing.SystemColors.MenuText;
+            this.userListView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.userListView.ForeColor = System.Drawing.Color.Red;
+            this.userListView.Location = new System.Drawing.Point(6, 107);
+            this.userListView.Name = "userListView";
+            this.userListView.Size = new System.Drawing.Size(290, 213);
+            this.userListView.TabIndex = 11;
+            this.userListView.UseCompatibleStateImageBehavior = false;
+            this.userListView.View = System.Windows.Forms.View.Details;
+            // 
+            // button5
+            // 
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.Red;
+            this.button5.Location = new System.Drawing.Point(302, 326);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(283, 50);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Unban Selected User";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(6, 326);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(290, 50);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Clear All Active Bets (User)";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(302, 382);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(283, 50);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Ban Selected User";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(6, 382);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(290, 50);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Clear All Active Bets (Whole Server)";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(7, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(578, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Connection Type :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // serverConnectionPicker
+            // 
+            this.serverConnectionPicker.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.serverConnectionPicker.BackColor = System.Drawing.SystemColors.MenuText;
+            this.serverConnectionPicker.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.serverConnectionPicker.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.serverConnectionPicker.ForeColor = System.Drawing.Color.Red;
+            this.serverConnectionPicker.FormattingEnabled = true;
+            this.serverConnectionPicker.Items.AddRange(new object[] {
+            "LocalHost",
+            "OnlineWebHost"});
+            this.serverConnectionPicker.Location = new System.Drawing.Point(7, 43);
+            this.serverConnectionPicker.Name = "serverConnectionPicker";
+            this.serverConnectionPicker.Size = new System.Drawing.Size(578, 24);
+            this.serverConnectionPicker.TabIndex = 0;
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(622, 591);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.connectToSeverButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "mainWindow";
+            this.Text = "Demon Slayers : Betting Server Manager";
+            this.Load += new System.EventHandler(this.mainWindow_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label connectionStateLabel;
+        private System.Windows.Forms.Label clientCountLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button connectToSeverButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox serverConnectionPicker;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView userListView;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ BetServerManager/BetServerManager/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace BetServerManager
+{
+   
+    public partial class mainWindow : Form
+    {
+        ServerConnection server = new ServerConnection();
+        
+
+        public mainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mainWindow_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void connectToSeverButton_Click(object sender, EventArgs e)
+        {
+            if(serverConnectionPicker.Text =="LocalHost")
+            {
+                //We are going to initalize connection to the localhost.
+                server.svLogin("localhost", "recipe_app", "root", "", "3306", "none");
+                try
+                {
+                    server.connection.Open();
+                    MessageBox.Show("Connection to " + server.serverName + " succesful");
+                    connectionStateLabel.Text = "Connected : "+ server.serverName;
+                    server.connection.Close();
+                }
+                catch (MySqlException err)
+                {
+                    MessageBox.Show(err.Message + server.serverName);
+                    connectionStateLabel.Text = "Failed to connect to " + server.serverName +" Consider checking XAMPP.";
+                }
+            }
+            if (serverConnectionPicker.Text == "OnlineWebHost")
+            {
+                MessageBox.Show("No Webhost has been set in config file, defaulting to localhost connection.");
+                //We are going to initalize connection to the localhost.
+                server.svLogin("localhost", "recipe_app", "root", "", "3306", "none");
+                try
+                {
+                    server.connection.Open();
+                    MessageBox.Show("Connection to " + server.serverName + " succesful");
+                    server.connection.Close();
+                }
+                catch (MySqlException err)
+                {
+                    MessageBox.Show(err.Message + server.serverName);
+                    connectionStateLabel.Text = "Failed to connect to " + server.serverName + " Consider checking XAMPP.";
+                }
+            }
+        }
+
+    }
+}
