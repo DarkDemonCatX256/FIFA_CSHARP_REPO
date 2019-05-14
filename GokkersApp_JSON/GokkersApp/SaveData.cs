015a650fa82751d1a57b3555fe45a332f22728da
The GIT SLAYER <44425998+DarkDemonCatX256@users.noreply.github.com>
2019-05-14 10:38:40 +0200

    Add files via upload

---
+++ GokkersApp_JSON/GokkersApp/SaveData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GokkersApp
+{
+    class SaveData
+    {
+        public int wins;
+        public int losses;
+        public int points;
+    }
+}
